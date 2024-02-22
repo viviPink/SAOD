65b7fb3de0ffa2520b9ef574410bf6da59737fc3
viviPink <124152971+viviPink@users.noreply.github.com>
2024-02-22 16:18:20 +0900

    new

---
+++ dataAnalysis.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+#include <fstream>
+#include <cctype>
+#include "dataAnalysis.h"
+
+int main() {
+    //проверка работы функции
+    AssertU();
+
+    size_t countUp = CountUppercase("input.txt");
+    cout << "Number of uppercase letters: " << countUp << endl;
+
+    OutFile("output.txt", countUp);
+}
