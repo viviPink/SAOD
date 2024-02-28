94707311a7ed84e0da1cdbd9042e625bc833ab72
viviPink <124152971+viviPink@users.noreply.github.com>
2024-02-28 10:38:00 +0900

    new

---
+++ RecursAdd.h
@@ -0,0 +1,24 @@
+#ifndef RECURSADD_H
+#define RECURSADD_H
+#include <iostream>
+#include <fstream>
+#include <cassert>
+
+//функция для поиска суммы чисел a и b с помощью прибавления 1 и вычитания 1  (возращает значение результата сложения )
+template<typename T>
+T recAdd(T a, T b)
+{
+    if (a == 0)
+        return b;
+    // вызываем рекурсию пока первый агрумент не будет равен 0
+    return recAdd(a - 1, b + 1);
+}
+
+// Проверка работы функции recAdd
+void TestAssert()
+{
+    assert(recAdd(7, 3) == 10);
+    assert(recAdd(9, 3) == 12);
+    assert(recAdd(6, 8) == 14);
+}
+#endif
---
+++ Recursion.cpp
@@ -0,0 +1,35 @@
+﻿
+//Gertruda Pinchukova 
+#include <iostream>
+#include <cassert>
+#include "RecursAdd.h"
+using namespace std;
+
+
+
+int main(int argc, char* argv[])
+{
+    // для ввода с командной строки
+    if (argc == 3)
+    { //функцию atoi для преобразования аргументов командной строки в целочисленные значения
+        int a = atoi(argv[1]);
+        int b = atoi(argv[2]);
+
+        int result = recAdd(a, b);
+        cout << "Сумма: " << result << endl;
+    }
+    //для ввода с консоли 
+    else
+    {
+        TestAssert();
+
+        int a, b;
+        cout << "A: ";
+        cin >> a;
+        cout << "B: ";
+        cin >> b;
+        int result = recAdd(a, b);
+        cout << "Summ: " << result << endl;
+    }
+}
+
